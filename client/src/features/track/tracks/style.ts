70d71bb5cad54de9dbadda09d63f54fe7408d7ac
aruzo <64381826+aruzo1@users.noreply.github.com>
2022-09-24 12:13:35 +0200

    updated tracks grid

--- client/src/features/track/tracks/style.ts
+++ client/src/features/track/tracks/style.ts
@@ -3,19 +3,19 @@ import { devices } from "styles/theme";
 
 export const StyledTracks = styled.ul`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   gap: 1rem;
 
   @media (max-width: ${devices.lg}) {
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(5, 1fr);
   }
 
   @media (max-width: ${devices.md}) {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
   }
 
   @media (max-width: ${devices.sm}) {
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   }
 
   @media (max-width: ${devices.xs}) {
