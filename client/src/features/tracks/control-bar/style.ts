f9b331b0b1b6294620b48d443069f4af61784363
aruzo <dawid12391111@gmail.com>
2022-10-26 17:47:00 +0200

    updated layout

--- client/src/features/tracks/control-bar/style.ts
+++ client/src/features/tracks/control-bar/style.ts
@@ -3,6 +3,9 @@ import theme from "styles/theme";
 import Container from "components/container";
 
 export const StyledControlBar = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
   padding: 1rem 0;
   background: ${theme.colors.neutral[800]};
   animation: slide-up ${theme.transition};
