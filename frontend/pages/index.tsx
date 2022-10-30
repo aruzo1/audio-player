eedb8e4c196bb60091ebdea4e3235be85edd6f1a
aruzo <dawid12391111@gmail.com>
2022-10-30 23:06:11 +0100

    #5 initialized next app

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Audio Player | Home</title>
+      </Head>
+      xxx
+    </div>
+  );
+}
+
+export default Home;
